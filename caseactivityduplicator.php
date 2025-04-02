c9eacad635f4caa2d4e5eee94955b4c461906dbc
agileware-fj <agileware-fj@users.noreply.github.com>
2025-04-02 18:42:42 +0000

    CIVIBLD-287 [AUTO] civix upgraded
    Files changed:
    M	caseactivityduplicator.civix.php
    M	caseactivityduplicator.php
    M	info.xml
    A	mixin/smarty-v2@1.0.3.mixin.php

--- caseactivityduplicator.php
+++ caseactivityduplicator.php
@@ -22,59 +22,12 @@ function caseactivityduplicator_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function caseactivityduplicator_civicrm_postInstall() {
-  _caseactivityduplicator_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function caseactivityduplicator_civicrm_uninstall() {
-  _caseactivityduplicator_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function caseactivityduplicator_civicrm_enable() {
   _caseactivityduplicator_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function caseactivityduplicator_civicrm_disable() {
-  _caseactivityduplicator_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function caseactivityduplicator_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _caseactivityduplicator_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
- */
-function caseactivityduplicator_civicrm_entityTypes(&$entityTypes) {
-  _caseactivityduplicator_civix_civicrm_entityTypes($entityTypes);
 }
 
 /**
